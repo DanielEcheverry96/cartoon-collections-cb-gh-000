88815196142c9a361f2d4fce5fb52921f45fae37
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-02-05 03:23:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,11 +11,11 @@ def summon_captain_planet(planeteer_calls)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  array.any? { |item| item.length > 4  }
+  array.any? { |item| item.length > 4 }
 end
 
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find {|item|cheese_types.include?(item)}
+  array.find {|item| cheese_types.include?(item) }
 end
