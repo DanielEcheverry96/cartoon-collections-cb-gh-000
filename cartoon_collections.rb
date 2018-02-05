d520ef592f758678ffc96239bccb001c1603ddd5
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-02-05 03:11:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   array.each do |item|
-    return item.length > 4 ? true : false
+     item.length > 4 ? return true : return false
   end
 end
 
