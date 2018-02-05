48d2bb724f22d270684736e0b6bc2e9891a4ab7b
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-02-05 03:15:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,9 +11,7 @@ def summon_captain_planet(planeteer_calls)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  array.each do |item|
-    return item.length > 4 ? true : false
-  end
+  array.any? { |item| item.length > 4  }
 end
 
 def find_the_cheese# code an argument here
