f135675a1466ac4441fafb953d8802a555b238a1
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-02-05 02:41:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   planeteer_calls.map do |planeteer|
-    planeteer.capitalize
-    
+    planeteer.capitalize + "!"
+
   end
 end
 
